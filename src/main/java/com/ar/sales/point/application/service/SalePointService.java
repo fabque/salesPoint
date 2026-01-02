ddd6c95573c0c8892f4344e6fa827c9f101cf7cd
Fabricio Quevedo <fabque@gmail.com>
2026-01-02 16:17:12 -0300

    salepoint adding cache

--- src/main/java/com/ar/sales/point/application/service/SalePointService.java
+++ src/main/java/com/ar/sales/point/application/service/SalePointService.java
@@ -3,6 +3,9 @@ package com.ar.sales.point.application.service;
 import com.ar.sales.point.application.port.in.SalePointUseCase;
 import com.ar.sales.point.application.port.out.SalePointRepositoryPort;
 import com.ar.sales.point.domain.model.SalePoint;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,21 +20,25 @@ public class SalePointService implements SalePointUseCase {
     }
 
     @Override
+    @CacheEvict(value = "salePoints", allEntries = true)
     public SalePoint createSalePoint(SalePoint salePoint) {
         return salePointRepositoryPort.save(salePoint);
     }
 
     @Override
+    @Cacheable(value = "salePoints", key = "#id")
     public SalePoint getSalePointById(Long id) {
         return salePointRepositoryPort.findById(id);
     }
 
     @Override
+    @Cacheable("salePoints")
     public List<SalePoint> getAllSalePoints() {
         return salePointRepositoryPort.findAll();
     }
 
     @Override
+    @CacheEvict(value= "salePoints", allEntries = true)
     public void deleteSalePoint(Long id) {
         salePointRepositoryPort.deleteById(id);
     }
