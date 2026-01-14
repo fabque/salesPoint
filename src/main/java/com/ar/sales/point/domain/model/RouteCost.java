7137ea386eab21c2c82e5843077133c2656a714c
Fabricio Quevedo <fabque@gmail.com>
2026-01-14 16:31:26 -0300

    Revertir a clase RouteCost

--- src/main/java/com/ar/sales/point/domain/model/RouteCost.java
+++ src/main/java/com/ar/sales/point/domain/model/RouteCost.java
@@ -2,5 +2,12 @@ package com.ar.sales.point.domain.model;
 
 import java.util.List;
 
-public record RouteCost(List<Long> path, double cost) {
+public class RouteCost{
+    private List<Long> path;
+    private double cost;
+
+    public RouteCost(List<Long> path, double cost) {
+        this.path = path;
+        this.cost = cost;
+    }
 }
